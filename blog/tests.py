e73058417c44098ad78f403ce5430895e65ab286
Dominykas <d.gudauskas1@gmail.com>
2020-08-29 15:05:20 +0300

    First successful non-trivial test resolving the home_page

--- blog/tests.py
+++ blog/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
 from blog.views import home_page
+from django.http import HttpRequest
 
 
 class HomePageTest(TestCase):
@@ -8,3 +9,11 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<!DOCTYPE html>'))
+        self.assertIn('<title>Beep Boop Dom</title>', html)
+        self.assertTrue(html.endswith('</html>'))
