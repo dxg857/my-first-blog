85f65cd539a2ad4b27ba713b2063742dca4d4c56
Dominykas <d.gudauskas1@gmail.com>
2020-08-29 15:40:01 +0300

    Functional Test now logs in as admin as searches for the addPost button

--- blog/functional_tests.py
+++ blog/functional_tests.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,13 +18,31 @@ class NewVisitorTest(unittest.TestCase):
 
         # Here we see the title is "Beep Boop Dom", which is a natural indicator of a programmer
         self.assertIn('Beep Boop Dom', self.browser.title)
-        self.fail('Finish the test!')
 
         # Once we're on the home page, we can see a few posts already made by me, with the latest
         # one being titled "Working with Django"
+        post_title = self.browser.find_element_by_id('post_title').text
+        self.assertIn('Working with Django', post_title)
 
         # We actually have to log in as an administrator of the blog in order to create new posts
-        # And once we do we should see the "Add Post" button in the bottom left corner of the screen
+        self.browser.get('http://localhost:8000/admin')
+        username_box = self.browser.find_element_by_id('id_username')
+        password_box = self.browser.find_element_by_id('id_password')
+
+        # We type in the username and password for the admin account and hit enter to log in
+        username_box.send_keys('Rummeris')
+        password_box.send_keys('vdddnknv')
+        password_box.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # Just making sure we've successfully logged onto an admin account
+        administration = self.browser.find_element_by_id('site-name').text
+        self.assertIn('Django administration', administration)
+
+        # After we log in we should go back to the home page of the blog and see the "Add Post" button in the bottom
+        # left corner of the screen
+        self.browser.get('http://localhost:8000')
+        add_post_button = self.browser.find_element_by_id('add_post_button')
 
         # We try adding a new post with the title "Test Post" and a randomly generated sha-256 hash
         # as its text
