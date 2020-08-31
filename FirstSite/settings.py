caa33587bdb9f897945b44a290848678cac37b81
Dominykas <d.gudauskas1@gmail.com>
2020-08-31 21:23:35 +0300

    Trying to run with Debug mode off

--- FirstSite/settings.py
+++ FirstSite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(ggj#e)_=gi1t=h4#%*am79**!ch2@!%a=sy=u+6ej3!ht)ql_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'localhost']
 
