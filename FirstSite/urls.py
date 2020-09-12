ac6d7b4863dc80ca53dd109015e2de1b25193c2d
Dominykas <d.gudauskas1@gmail.com>
2020-09-12 19:46:20 +0300

    Trying to fix an issue that appeared after introducing 'trix' to pythonanywhere

--- FirstSite/urls.py
+++ FirstSite/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.staticfiles.storage import staticfiles_storage
@@ -23,5 +22,4 @@ urlpatterns = [
     path('', include('blog.urls')),
     path('admin/', admin.site.urls),
     path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('assets/images/favicon.ico'))),
-    url(r'^trixorwhateveryouwant/', include('trix.urls')),
 ]
